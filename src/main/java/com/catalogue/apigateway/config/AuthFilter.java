e83b7b3573b7a432069004ab3e0379001ff332bb
Okayreet <darkerror@hotmail.co.uk>
2023-04-13 17:29:36 +0100

    working filters

--- src/main/java/com/catalogue/apigateway/config/AuthFilter.java
+++ src/main/java/com/catalogue/apigateway/config/AuthFilter.java
@@ -1,19 +1,13 @@
 package com.catalogue.apigateway.config;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 import com.catalogue.apigateway.exception.AppException;
 
@@ -38,60 +32,27 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
         return (exchange, chain) -> {
             log.info("Into exchange!");
             ServerHttpRequest request = exchange.getRequest();
-            ServerHttpResponse response = exchange.getResponse();
-            response.getHeaders().set(VALUE_KEY, NAME_KEY);
-            String target = exchange.getRequest().getURI().toString();
 
-            return redirectToLogin(exchange, chain);
-            // exchange.getAttributes().put(GATEWAY_PREDICATE_MATCHED_PATH_ATTR,
-            // "/api/v1/auth/login");
-            // return chain.filter(exchange);
-            // System.out.println("target is: " + target);
-            // if (isAuthMissing(request))
-            // return onError(exchange, "Authorization header is missing in request",
-            // HttpStatus.UNAUTHORIZED);
+            if (isAuthMissing(request))
+                return onError(exchange, "Authorization header is missing in request",
+                        HttpStatus.UNAUTHORIZED);
 
-            // log.info("Authorization header exists!");
-            // String token = getAuthHeader(request);
+            log.info("Authorization header exists!");
+            String token = getAuthHeader(request);
 
             // if (token.startsWith("Basic") && !target.endsWith("auht/login"))
             // return chain.filter(exchange);
-            // if (!token.startsWith("Bearer"))
-            // return onError(exchange, "Authorization header not Bearer",
-            // HttpStatus.UNAUTHORIZED);
-            // token = token.substring(7);
 
-            // if (jwtUtil.isInvalid(token))
-            // populateRequestWithHeaders(exchange, token);
-            // // return this.onError(exchange, "Authorization header is invalid",
-            // HttpStatus.UNAUTHORIZED);
+            if (!token.startsWith("Bearer") || jwtUtil.isInvalid(token.substring(7)))
+                return onError(exchange, "Authorization header is ivalid, please login",
+                        HttpStatus.UNAUTHORIZED);
 
-            // return chain.filter(exchange);
+            token = token.substring(7);
+
+            populateRequestWithHeaders(exchange, token);
+
+            return chain.filter(exchange);
         };
-    }
-
-    private Mono<Void> redirectToLogin(ServerWebExchange exchange, GatewayFilterChain chain) {
-
-        URI redirectURI = null;
-        try {
-            redirectURI = new URI("http://localhost:8084/api/v1/auth/login");
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        ServerHttpRequest modifiedRequest = exchange
-                .getRequest()
-                .mutate()
-                .uri(redirectURI)
-                .build();
-
-        ServerWebExchange modifiedExchange = exchange
-                .mutate()
-                .request(modifiedRequest)
-                .build();
-
-        modifiedExchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, redirectURI);
-
-        return chain.filter(modifiedExchange);
     }
 
     private void populateRequestWithHeaders(ServerWebExchange exchange, String token) {
